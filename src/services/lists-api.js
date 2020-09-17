27148d939e8066917843d1d62b791072e40faf00
kkonkel23 <kailahkonkel@gmail.com>
2020-09-17 15:41:33 -0400

    add delete functionality

--- src/services/lists-api.js
+++ src/services/lists-api.js
@@ -11,4 +11,10 @@ export function create(list) {
       headers: {'content-type': 'application/json'},
       body: JSON.stringify(list)
     }).then(res => res.json());
-  }
+}
+
+export function deleteOne(id) {
+  return fetch(`${BASE_URL}/${id}`, {
+    method: 'DELETE'
+  }).then(res => res.json())
+}
